063a6cecabc2a6850489ff38f040b2e82abd0948
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-03-19 20:09:26 +0530

    Add searchUser and deleteAll functions in queries.js for enhanced username management

--- db/queries.js
+++ db/queries.js
@@ -9,7 +9,19 @@ async function insertUsername(username) {
   await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
 }
 
+async function searchUser(user){
+  const {rows} = await pool.query("SELECT * FROM usernames WHERE username ILIKE '%' || $1 || '%'", [user]);
+  console.log(rows);
+  return rows;
+}
+
+async function deleteAll() {
+  await pool.query("DELETE FROM usernames");
+}
+
 module.exports = {
   getAllUsernames,
-  insertUsername
+  insertUsername,
+  searchUser,
+  deleteAll
 };
