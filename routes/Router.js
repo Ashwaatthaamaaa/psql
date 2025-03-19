e8c652394def45bc4c7d7b5e638b4f10fdf638dc
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-03-19 20:09:37 +0530

    Add deleteAll endpoint in Router.js to enable clearing all usernames

--- routes/Router.js
+++ routes/Router.js
@@ -5,6 +5,5 @@ const usersRouter = Router();
 usersRouter.get('/', usersController.getUsernames);
 usersRouter.get('/new', usersController.createUsernameGet);
 usersRouter.post('/new', usersController.createUsernamePost);
-
-
+usersRouter.get('/delete',usersController.deleteAll)
 module.exports = usersRouter;
