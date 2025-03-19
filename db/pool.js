0b5cef9a62dc7ac8cc3350349789af450009d60a
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-03-19 20:07:45 +0530

    Refactor database connection configuration in pool.js to use a connection string format for PostgreSQL.

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,5 @@ const { Pool } = require("pg");
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: "lunge",
-  database: "top_users",
-  password: "291152",
-  port: 5432 // The default port
+  connectionString: "postgresql://lunge:291152@localhost:5432/top_users",
 });
